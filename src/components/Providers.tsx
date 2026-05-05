d07ff555b58aba698569a1f11b5d216c3f8f3c3f
Aneesh Krishna <aneesh.krishna@aykansoft.com>
2026-05-05 23:12:50 +0530

    live trips removed

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,15 +2,12 @@
 
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "sonner";
-import { GoogleMapsLoader } from "@/lib/googleMapsLoader";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      <GoogleMapsLoader>
-        {children}
-        <Toaster position="top-center" richColors />
-      </GoogleMapsLoader>
+      {children}
+      <Toaster position="top-center" richColors />
     </SessionProvider>
   );
 }
